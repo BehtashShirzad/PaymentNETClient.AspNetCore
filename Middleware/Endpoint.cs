679dc44a92572a61230dd3d6c8400a07e432fe12
Mohamad <Mohamadshirzad81@gmail.com>
2026-07-18 20:09:51 +0330

    add init

---
+++ Middleware/Endpoint.cs
@@ -0,0 +1,16 @@
+﻿namespace PaymentNET.AspNetCore.Middleware;
+
+public static class Endpoint
+{
+    public static void MapPaymentVerificationEndpoint(this IEndpointRouteBuilder  app)
+    {
+        var group = app.MapGroup("transaction-verification");
+        group.MapPost("verify", VerifyTransactionCallBack);
+    }
+
+    private static Task VerifyTransactionCallBack()
+    {
+        
+        return Task.CompletedTask;
+    }
+}
