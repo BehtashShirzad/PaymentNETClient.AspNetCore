80c1fcde7a1aaeb02630d1ccf6cc6fe1e01ac11f
Mohamad <Mohamadshirzad81@gmail.com>
2026-07-24 16:25:04 +0330

    fix Endpoint.cs

--- Middleware/Endpoint.cs
+++ Middleware/Endpoint.cs
@@ -11,7 +11,7 @@ public static class Endpoint
     public static void MapPaymentVerificationEndpoint(this IEndpointRouteBuilder  app)
     {
         var group = app.MapGroup("transaction-verification");
-        group.MapPost("verify/{referenceId}", VerifyTransactionCallBack);
+        group.MapGet("verify/{referenceId}", VerifyTransactionCallBack);
     }
 
     private static async Task VerifyTransactionCallBack([FromRoute] string referenceId ,[FromServices]IPaymentVerificationHandler paymentVerificationHandler,CancellationToken cancellationToken)
